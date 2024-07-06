ef83e529f32f0613c2c73664bbcfaa10658e9fa1
Reechee <yangrichie528@gmail.com>
2024-07-06 15:43:48 +1000

    randomly generating walls

--- tank-troublee/src/main.cpp
+++ tank-troublee/src/main.cpp
@@ -13,13 +13,60 @@ float f(int x) {
     return float(x + 3);
 }
 
+int dsu[69];
+int leader(int x) {
+    if (dsu[x] == x) return x;
+    return dsu[x] = leader(dsu[x]);
+}
+
 int generateMap() {
-    // OUTER WALLS
-    for(int i = 0; i < 8; i++) {
-        wallsY[i][0] = wallsY[i][8] = 1;
-        wallsX[0][i] = wallsX[8][i] = 1;
+    // Generate map randomly
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 9; j++) {
+            wallsY[i][j] = std::rand() % 2;
+            if (j == 0 || j == 8) wallsY[i][j] = 1;
+        }
     }
-    // HORIZONTAL WALLS
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 8; j++) {
+            wallsX[i][j] = std::rand() % 2;
+            if (i == 0 || i == 8) wallsX[i][j] = 1;
+        }
+    }
+    // Make sure map is connected
+    std::vector<std::pair<int, int>> edges;
+    for (int i = 0; i < 64; i++) dsu[i] = i;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 1; j < 8; j++) {
+            int a = i * 8 + j - 1;
+            int b = i * 8 + j;
+            if (wallsY[i][j]) edges.push_back({a, b});
+            else dsu[leader(a)] = leader(b);
+        }
+    }
+    for (int i = 1; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            int a = j * 8 + i - 1;
+            int b = j * 8 + i;
+            if (wallsY[i][j]) edges.push_back({a, b});
+            else dsu[leader(a)] = leader(b);
+        }
+    }
+    std::random_shuffle(edges.begin(), edges.end());
+    for (std::pair<int, int> edge : edges) {
+        int a = edge.first;
+        int b = edge.second;
+        if (leader(a) != leader(b)) {
+            /*
+            
+            REMOVE THE WALL BETWEEN A AND B SOMEHOW
+            
+            */
+            dsu[leader(a)] = leader(b);
+        }
+    }
+    // Create rectangles
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 9; j++) {
             if (wallsY[i][j]) {
@@ -33,7 +80,6 @@ int generateMap() {
             }
         }
     }
-    // VERTICAL WALLS
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 8; j++) {
             if (wallsX[i][j]) {
